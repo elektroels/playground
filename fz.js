84c17f1398b67888aa03166afc7264ad5e659736
Troels Henningsen <elektroels@gmail.com>
2022-08-30 22:01:10 +0200

    fizzbuzz'nnnn

---
+++ fz.js
@@ -0,0 +1,19 @@
+// Ex: node fz.js 20
+let x = parseInt(process.argv[2])
+
+// Range fordi det er nemt
+let range = r => [...Array(r).keys()]
+
+// Kevlin Henney fizz buzz nu med 0 check
+let fz = (n) => {
+	let test = (d, s, x) => 
+		//n % d == 0 ? _ => s + x("") : x // uden 0 check
+		n == 0 ? x : n % d == 0 ? _ => s + x("") : x // med 0 check
+
+	let fizz = x => test(3, "Fizz", x)
+	let buzz = x => test(5, "Buzz", x)
+
+	return fizz(buzz(x => x))(n.toString())}
+
+range(x).forEach(x => console.log(fz(x)))
+
