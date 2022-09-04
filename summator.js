32eaaf00c9df748a08a5159e68de3653b8f63d6e
Troels Henningsen <elektroels@gmail.com>
2022-09-04 20:34:01 +0200

    chaining

---
+++ summator.js
@@ -0,0 +1,22 @@
+
+function summator(input) {
+	this.$this = this
+	$this.sum = 0
+	
+	$this.sum = input + $this.sum
+
+	$this.chain = (input) => {
+		$this.sum = input + $this.sum
+		return $this
+	}
+
+	$this.result = () => {
+		console.log($this.sum)
+		return $this
+	}
+
+	return $this
+}
+
+summator(1).chain(2).result()
+
