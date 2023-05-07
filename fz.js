db2d3ecadc8e1078b3e12aa8d9760cae356a967e
Troels Henningsen <elektroels@gmail.com>
2023-05-07 10:35:02 +0200

    fz upd

--- fz.js
+++ fz.js
@@ -1,18 +1,16 @@
-const lib = require('./lib');
+const range = r => [...Array(r).keys()]
 
-// Ex: node fz.js 20
 let x = parseInt(process.argv[2])
 
-// Kevlin Henney fizz buzz nu med 0 check
 let fz = (n) => {
-	let test = (d, s, x) => 
-		//n % d == 0 ? _ => s + x("") : x // uden 0 check
-		n == 0 ? x : n % d == 0 ? _ => s + x("") : x // med 0 check
+	//let test = n % d == 0 ? _ => s + x("") : x // uden 0 check
+	let test = (d, s, x) => n == 0 ? x : n % d == 0 ? _ => s + x("") : x
 
 	let fizz = x => test(3, "Fizz", x)
 	let buzz = x => test(5, "Buzz", x)
 
 	return fizz(buzz(x => x))(n.toString())}
 
-lib.range(x).forEach(x => console.log(fz(x)))
+
+range(x).forEach(x => console.log(fz(x)))
 
